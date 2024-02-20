e06a21e7bc89ef295cac06db4b57396a2b95a7ca
Rundus <connorfeltman@yahoo.com>
2024-02-20 09:40:19 -0600

    github website update 6

--- extra/header.js
+++
@@ -1,8 +0,0 @@
-document.write("<div class=\"Header\">");
-document.write("    <a href=\"https:\/\/physics.uiowa.edu\/\" ><img src=\"\/photos\/logos\/DomeWordSingleGOLD.png\" alt=\"University of Iowa Logo\"><\/a>");
-document.write("<\/div>");
-
-
-//<div class="Header">
-//    <a href="https://physics.uiowa.edu/" ><img src="/photos/logos/DomeWordSingleGOLD.png" alt="University of Iowa Logo"></a>
-//</div>
---
+++ js/header.js
@@ -0,0 +1,8 @@
+document.write("<div class=\"Header\">");
+document.write("    <a href=\"https:\/\/physics.uiowa.edu\/\" ><img src=\"\/photos\/logos\/blockIOWA_yellow.png\" alt=\"University of Iowa Logo\"><\/a>");
+document.write("<\/div>");
+
+//
+//<div class="Header">
+//    <a href="https://physics.uiowa.edu/" ><img src="/photos/logos/blockIOWA_yellow.png" alt="University of Iowa Logo"></a>
+//</div>
