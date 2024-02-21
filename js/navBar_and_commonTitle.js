6c278f11ab2acf690d8886f0e3758f2bcb199478
Rundus <connorfeltman@yahoo.com>
2024-02-21 09:47:14 -0600

    github website update 7

---
+++ js/navBar_and_commonTitle.js
@@ -0,0 +1,60 @@
+document.write("<div class=\"commonTitle\">");
+document.write("    <div class=\"commonPageTitle\">");
+document.write("        <h1>Connor A. Feltman<\/h1>");
+document.write("        <h3>");
+document.write("            Ph.D. candidate");
+document.write("            <br>");
+document.write("            The University of Iowa");
+document.write("        <\/h3>");
+document.write("    <\/div>");
+document.write("");
+document.write("    <div class = \"navigation\">");
+document.write("        <p>");
+document.write("            <a href=\"\/HTML\/index.html\">Home<\/a>");
+document.write("            |");
+document.write("            <a href=\"\/HTML\/pages\/research\/index.html\">Research<\/a>");
+document.write("            |");
+document.write("            <a href=\"\/HTML\/pages\/publications\/index.html\">Publications<\/a>");
+document.write("            |");
+document.write("            <a href=\"\/HTML\/pages\/teaching\/index.html\">Teaching<\/a>");
+document.write("            |");
+document.write("            <a href=\"\/HTML\/pages\/gallery\/index.html\">Gallery<\/a>");
+document.write("            |");
+document.write("            <a href=\"\/HTML\/pages\/contact\/index.html\">Contact<\/a>");
+document.write("        <\/p>");
+document.write("    <\/div>");
+document.write("<\/div>");
+
+
+
+
+
+
+
+
+//<div class="commonTitle">
+//    <div class="commonPageTitle">
+//        <h1>Connor A. Feltman</h1>
+//        <h3>
+//            Ph.D. candidate
+//            <br>
+//            The University of Iowa
+//        </h3>
+//    </div>
+//
+//    <div class = "navigation">
+//        <p>
+//            <a href="/HTML/index.html">Home</a>
+//            |
+//            <a href="/HTML/pages/research/index.html">Research</a>
+//            |
+//            <a href="/HTML/pages/publications/index.html">Publications</a>
+//            |
+//            <a href="/HTML/pages/teaching/index.html">Teaching</a>
+//            |
+//            <a href="/HTML/pages/gallery/index.html">Gallery</a>
+//            |
+//            <a href="/HTML/pages/contact/index.html">Contact</a>
+//        </p>
+//    </div>
+//</div>
