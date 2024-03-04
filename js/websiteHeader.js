3f9f03ccc50a45acac626bae28a89f929aaf8c8a
Rundus <connorfeltman@yahoo.com>
2024-03-04 10:05:52 -0600

    github website update 19

---
+++ js/websiteHeader.js
@@ -0,0 +1,12 @@
+document.write("<meta charset=\"UTF-8\">");
+document.write("<link rel=\"icon\" type=\"image\/x-icon\" href=\"\/HTML\/photos\/logos\/favicon.png\">");
+document.write("<link href = \"\/HTML\/css\/custom.css\" rel=\"stylesheet\">");
+document.write("<script type=\"text\/javascript\"  src=\"http:\/\/cdn.mathjax.org\/mathjax\/latest\/MathJax.js?config=TeX-AMS-MML_HTMLorMML\"> <\/script>");
+
+
+
+
+//<meta charset="UTF-8">
+//<link rel="icon" type="image/x-icon" href="/HTML/photos/logos/favicon.png">
+//<link href = "/HTML/css/custom.css" rel="stylesheet">
+//<script type="text/javascript"  src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"> </script>
