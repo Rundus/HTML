5981d19e45930146070f7d1fc7f8fbe79ffe68c7
Rundus <connorfeltman@yahoo.com>
2024-03-06 10:12:55 -0600

    github website update 23

--- js/copyright.js
+++ js/copyright.js
@@ -1,6 +1,6 @@
 document.write("<div class=\"copyright\">");
 document.write("    <p>");
-document.write("        Copyright @ 2019 All Rights Reserved | Design by Connor A. Feltman");
+document.write("        Copyright @ 2019 - 2024 All Rights Reserved | Design by Connor A. Feltman");
 document.write("    <\/p>");
 document.write("<\/div>");
 
@@ -8,6 +8,6 @@ document.write("<\/div>");
 
 //<div class="copyright">
 //    <p>
-//        Copyright @ 2019 All Rights Reserved | Design by Connor A. Feltman
+//        Copyright @ 2019 - 2024 All Rights Reserved | Design by Connor A. Feltman
 //    </p>
 //</div>
